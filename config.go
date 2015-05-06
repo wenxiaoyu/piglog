98d47f0afabdf054b0309b26071ae9ae11b45fc5
wenxy <wenxy@ucweb.cn>
2015-05-06 11:52:57 +0800

    增加Http协议接口，远程推送日志

--- config.go
+++ config.go
@@ -13,13 +13,17 @@
 package piglog
 
 import (
+	md5 "crypto/md5"
+	hex "encoding/hex"
 	xml "encoding/xml"
+	"fmt"
 	io "io/ioutil"
+	http "net/http"
 	"os"
 	exec "os/exec"
 	filepath "path/filepath"
+	"strconv"
 	"strings"
-	 
 )
 
 var configFileName = "piglog.xml"
@@ -36,6 +40,7 @@ const (
 
 type Config struct {
 	Piglog []PigLogConfig
+	Remote RemoteConfig
 }
 
 type PigLogConfig struct {
@@ -51,7 +56,14 @@ type SplitConfig struct {
 	Bysize int
 }
 
+type RemoteConfig struct {
+	Ip        string
+	Port      int
+	SecretKey string
+}
+
 var configmap = make(map[string]PigLogConfig)
+var config Config
 
 func init() {
 	err := loadConfig()
@@ -62,16 +74,20 @@ func init() {
 
 //load an xml of log config
 func loadConfig() (err error) {
- 	file, err := os.Open(configFileName)
+	file, err := os.Open(configFileName)
 
 	if err == nil {
 		b, e := io.ReadAll(file)
 
 		if e == nil {
-			var config Config
 			xml.Unmarshal(b, &config)
 			for _, plog := range config.Piglog {
 				configmap[plog.Event] = plog
+			}
+			if config.Remote.Ip != "" && config.Remote.Port > 0 {
+				fmt.Print("start http service : ")
+				fmt.Println(config.Remote)
+				startHttpServer(config.Remote)
 			}
 		} else {
 			return e
@@ -98,4 +114,56 @@ func GetCurrentPath() string {
 	index := strings.LastIndex(path, string(os.PathSeparator))
 	ret := path[:index]
 	return ret
+}
+
+func startHttpServer(remote RemoteConfig) {
+	http.HandleFunc("/log", httpLogServer)
+	iport := remote.Ip + ":" + strconv.Itoa(remote.Port)
+	err := http.ListenAndServe(iport, nil)
+
+	if err != nil {
+		fmt.Errorf("ListenAndServe:", err)
+	}
+}
+
+// handler log server.
+func httpLogServer(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	event := req.FormValue("event")
+	log := req.FormValue("log")
+	clientSign := req.FormValue("sign")
+	logtype := req.FormValue("type")
+	if event == "" || log == "" || clientSign == "" {
+		rw.Write([]byte("params losed."))
+	}
+	if validateSign(event, log, clientSign) {
+		switch logtype {
+		case "trace":
+			Log.Trace(event, log)
+		case "debug":
+			Log.Debug(event, log)
+		case "info":
+			Log.Info(event, log)
+		case "warn":
+			Log.Warn(event, log)
+		case "error":
+			Log.Error(event, log)
+		}
+		rw.Write([]byte("ok"))
+	} else {
+		rw.Write([]byte("sign error."))
+	}
+}
+
+//validate the sign,signed by event log params
+func validateSign(event string, log string, clientSign string) bool {
+	configSecretkey := config.Remote.SecretKey
+	hasher := md5.New()
+	hasher.Write([]byte(event + log + configSecretkey))
+	computerSign := hex.EncodeToString(hasher.Sum(nil))
+   
+	if computerSign == clientSign{
+		return true
+	}
+	return false
 }
